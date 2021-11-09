168d42bf4b9a84bc2656721f35d5395f531a6736
Аскар <Tastybaev@mail.ru>
2021-11-09 22:56:59 +0500

    bot is done

--- homework.py
+++ homework.py
@@ -29,7 +29,7 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    """функция для отправки вообщений"""
+    """функция для отправки вообщений."""
     bot.send_message(chat_id=CHAT_ID, text=message)
 
 
