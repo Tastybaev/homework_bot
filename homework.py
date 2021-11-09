e40a8e933c1d7eda63db382d010703bdbf425d3f
Аскар <Tastybaev@mail.ru>
2021-11-09 19:55:58 +0500

    bot is done

--- homework.py
+++ homework.py
@@ -1,15 +1,25 @@
 import logging
 
-...
+from http import HTTPStatus
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-CHAT_ID = ...
+import requests
+import telegram
+import utils
+import os
 
-...
+PRACTICUM_TOKEN = 'AQAAAAA-IasbAAYckSaOJjEn-kDmukhNLA-_4NA'
+TELEGRAM_TOKEN = '2093395141:AAF3PGObc08tWDQ9cTrzC6GadcnP_ZCn4tc'
+CHAT_ID = 1454224325
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='program.log',
+    format='%(asctime)s, %(levelname)s, %(message)s'
+)
 
 RETRY_TIME = 300
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -17,38 +27,47 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена, в ней нашлись ошибки.'
 }
 
-
 def send_message(bot, message):
-    ...
-
+    bot.send_message(chat_id=CHAT_ID, text=message)
 
 def get_api_answer(url, current_timestamp):
-    ...
-
+    date = {'from_date': current_timestamp}
+    response = requests.get(url, headers=HEADERS, params=date)
+    if response.status_code == 200:
+        response = response.json()
+        return response
+    raise ValueError('что-то пошло не так!')
 
 def parse_status(homework):
-    verdict = ...
-    ...
-
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    verdict = HOMEWORK_STATUSES.get(homework_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-
 
 def check_response(response):
     homeworks = response.get('homeworks')
-    ...
-
+    homework_status = homeworks[0].get('status')
+    if 'homeworks' in response:
+        if homeworks:
+            if homework_status in HOMEWORK_STATUSES:
+                return homeworks
+            raise ValueError('Статус домашней работы незадокментирован!')
+        raise ValueError('Домашние работы отсутствуют!')
+    raise ValueError('Ошибка! Что-то не то с сайтом.')
 
 def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    ...
+    
     while True:
         try:
-            ...
+            response = get_api_answer(ENDPOINT, current_timestamp)
+            homework = check_response(response)
+            message = parse_status(homework)
             time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            bot.send_message(CHAT_ID, message)
             time.sleep(RETRY_TIME)
             continue
 
