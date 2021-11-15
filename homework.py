23c5f856e1953845c99ca563dfc381375d4079c8
Аскар <Tastybaev@mail.ru>
2021-11-15 15:24:32 +0500

    final

--- homework.py
+++ homework.py
@@ -1,41 +1,41 @@
 import logging
- 
+
 import requests
- 
+
 import telegram
- 
+
 import time
- 
+
 from dotenv import load_dotenv
- 
+
 load_dotenv()
- 
+
 PRACTICUM_TOKEN = 'AQAAAAA-IasbAAYckSaOJjEn-kDmukhNLA-_4NA'
 TELEGRAM_TOKEN = '2093395141:AAF3PGObc08tWDQ9cTrzC6GadcnP_ZCn4tc'
 TELEGRAM_CHAT_ID = '1454224325'
- 
+
 logging.basicConfig(
     level=logging.INFO,
     filename='main.log',
     format='%(asctime)s, %(name)s, %(levelname)s, %(message)s'
 )
- 
+
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
- 
+
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
- 
- 
+
+
 def send_message(bot, message):
     """Функция для отправки вообщений."""
     bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
- 
- 
+
+
 def get_api_answer(current_timestamp):
     """Делаю запрос к единственному эндпоинту API-сервиса."""
     timestamp = current_timestamp or int(time.time())
@@ -45,8 +45,8 @@ def get_api_answer(current_timestamp):
         response = response.json()
         return response
     raise ValueError('что-то пошло не так!')
- 
- 
+
+
 def check_response(response):
     """Проверяю ответ API на корректность."""
     if type(response) is not dict:
@@ -58,8 +58,8 @@ def check_response(response):
         return homeworks
         raise ValueError('Домашние работы отсутствуют!')
     raise ValueError('Ошибка! Что-то не то с сайтом.')
- 
- 
+
+
 def parse_status(homework):
     """Извлекаю из информации о домашней работе статус этой работы."""
     homework_name = homework.get('homework_name')
@@ -78,13 +78,13 @@ def check_tokens():
         if i is None:
             result = False
         return result
- 
- 
+
+
 def main():
     """Описываю основную логику работы программы."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
- 
+
     while True:
         try:
             response = get_api_answer(ENDPOINT, current_timestamp)
@@ -97,6 +97,6 @@ def main():
             time.sleep(RETRY_TIME)
             continue
  
- 
+
 if __name__ == '__main__':
     main()
