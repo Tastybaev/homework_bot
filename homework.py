6a1822d0f1814f0f46f76e9e0990378085916f41
Аскар <Tastybaev@mail.ru>
2021-11-15 15:21:25 +0500

    final

--- homework.py
+++ homework.py
@@ -1,38 +1,41 @@
 import logging
-
+ 
 import requests
-
+ 
 import telegram
-
+ 
 import time
-
-
+ 
+from dotenv import load_dotenv
+ 
+load_dotenv()
+ 
 PRACTICUM_TOKEN = 'AQAAAAA-IasbAAYckSaOJjEn-kDmukhNLA-_4NA'
 TELEGRAM_TOKEN = '2093395141:AAF3PGObc08tWDQ9cTrzC6GadcnP_ZCn4tc'
 TELEGRAM_CHAT_ID = '1454224325'
-
+ 
 logging.basicConfig(
     level=logging.INFO,
-    filename='program.log',
+    filename='main.log',
     format='%(asctime)s, %(name)s, %(levelname)s, %(message)s'
 )
-
+ 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
+ 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-
-
+ 
+ 
 def send_message(bot, message):
     """Функция для отправки вообщений."""
     bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-
-
+ 
+ 
 def get_api_answer(current_timestamp):
     """Делаю запрос к единственному эндпоинту API-сервиса."""
     timestamp = current_timestamp or int(time.time())
@@ -42,29 +45,31 @@ def get_api_answer(current_timestamp):
         response = response.json()
         return response
     raise ValueError('что-то пошло не так!')
-
-
+ 
+ 
 def check_response(response):
     """Проверяю ответ API на корректность."""
+    if type(response) is not dict:
+        raise TypeError()
     homeworks = response.get('homeworks')
-    homework_status = homeworks[0].get('status')
     if 'homeworks' in response:
-        if homeworks:
-            if homework_status in HOMEWORK_STATUSES:
-                return homeworks
-            raise ValueError('Статус домашней работы незадокументирован!')
+        for homework in homeworks:
+            homework_status = homework.get('status')
+        return homeworks
         raise ValueError('Домашние работы отсутствуют!')
     raise ValueError('Ошибка! Что-то не то с сайтом.')
-
-
+ 
+ 
 def parse_status(homework):
     """Извлекаю из информации о домашней работе статус этой работы."""
     homework_name = homework.get('homework_name')
-    homework_status = homework.get('status')
-    verdict = HOMEWORK_STATUSES.get(homework_status)
+    status = homework.get('status')
+    if status not in HOMEWORK_STATUSES:
+        raise KeyError()
+    verdict = HOMEWORK_STATUSES[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-
-
+ 
+ 
 def check_tokens():
     """Проверяю TOKEN на корректность."""
     token_list = [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
@@ -73,13 +78,13 @@ def check_tokens():
         if i is None:
             result = False
         return result
-
-
+ 
+ 
 def main():
     """Описываю основную логику работы программы."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-
+ 
     while True:
         try:
             response = get_api_answer(ENDPOINT, current_timestamp)
@@ -91,7 +96,7 @@ def main():
             bot.send_message(TELEGRAM_CHAT_ID, message)
             time.sleep(RETRY_TIME)
             continue
-
-
+ 
+ 
 if __name__ == '__main__':
     main()
